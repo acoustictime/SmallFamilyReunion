aa1096a782c1fe1ca29562635b91606d1c3b8897
James Small <superbaker@gmail.com>
2015-02-12 21:13:58 -0800

    Current Template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,22 @@
 $( document ).ready(function() {
-    
-    $("#submit").click(function() {
- 
+    var frameSrc = "<iframe src='https://docs.google.com/forms/d/1QJSUpOCFLZiZgEggnthwhTTz8lI7icZQWze-dy4CoUU/viewform?embedded=true' width='760' height='500' frameborder='0' marginheight='0' marginwidth='0'>Loading...</iframe>";
 
-$('#modalBody').html("<iframe src='https://docs.google.com/forms/d/1QJSUpOCFLZiZgEggnthwhTTz8lI7icZQWze-dy4CoUU/viewform?embedded=true' width='750' height='700' frameborder='0' marginheight='0' marginwidth='0'>Loading...</iframe>");
-    	$("#submitModal").modal("show");
+    $("#submit").click(function() {
+
+   		$('#submitModal').modal({show:true})
     
     });
     
-    $('#submitModal').on('hidden.bs.modal', function () {
- $('#modalBody').html("");
-
-  		$(this).data('bs.modal', null);
-})
+   
     
     
 });
+
+
+// entry.784094184	test
+
+// entry.203616030	email@email.com
+
+// https://docs.google.com/forms/d/1QJSUpOCFLZiZgEggnthwhTTz8lI7icZQWze-dy4CoUU/viewform?usp=send_form
+
+// window.open ('https://docs.google.com/forms/d/1QJSUpOCFLZiZgEggnthwhTTz8lI7icZQWze-dy4CoUU/formResponse?entry.784094184=TEST&entry.203616030=TEST#','_self',false)
